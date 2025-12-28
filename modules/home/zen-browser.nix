4ceb432a4b30e3fe42d2240c6fc3215610839f0e
mugdad <mugdad02@tutamail.com>
2025-12-28 20:56:57 +0300

    dd

--- modules/home/zen-browser.nix
+++ modules/home/zen-browser.nix
@@ -1,9 +1,10 @@
 {
+  inputs,
   pkgs,
   lib,
-  inputs,
   ...
-}: let
+}:
+let
   extension = shortId: guid: {
     name = guid;
     value = {
@@ -13,57 +14,67 @@
   };
 
   prefs = {
+    # Check these out at about:config
     "extensions.autoDisableScopes" = 0;
     "extensions.pocket.enabled" = false;
+    # ...
   };
 
   extensions = [
+    # To add additional extensions, find it on addons.mozilla.org, find
+    # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+    # Then go to https://addons.mozilla.org/api/v5/addons/addon/!SHORT_ID!/ to get the guid
     (extension "ublock-origin" "uBlock0@raymondhill.net")
+    # ...
   ];
 
-  # use the beta variant
-  zenPkg = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system};
-in {
+in
+{
   home.packages = [
-    zenPkg
-  ];
+    (pkgs.wrapFirefox
+      inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.zen-browser-unwrapped
+      {
+        extraPrefs = lib.concatLines (
+          lib.mapAttrsToList (
+            name: value: ''lockPref(${lib.strings.toJSON name}, ${lib.strings.toJSON value});''
+          ) prefs
+        );
 
-  home.file.".config/zen-browser/policies.json".text = builtins.toJSON {
-    DisableTelemetry = true;
-    ExtensionSettings = builtins.listToAttrs extensions;
-    SearchEngines = {
-      Default = "google";
-      Add = [
-        {
-          Name = "nixpkgs packages";
-          URLTemplate = "https://search.nixos.org/packages?query={searchTerms}";
-          IconURL = "https://wiki.nixos.org/favicon.ico";
-          Alias = "@np";
-        }
-        {
-          Name = "NixOS options";
-          URLTemplate = "https://search.nixos.org/options?query={searchTerms}";
-          IconURL = "https://wiki.nixos.org/favicon.ico";
-          Alias = "@no";
-        }
-        {
-          Name = "NixOS Wiki";
-          URLTemplate = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
-          IconURL = "https://wiki.nixos.org/favicon.ico";
-          Alias = "@nw";
-        }
-        {
-          Name = "noogle";
-          URLTemplate = "https://noogle.dev/q?term={searchTerms}";
-          IconURL = "https://noogle.dev/favicon.ico";
-          Alias = "@ng";
-        }
-      ];
-    };
-  };
+        extraPolicies = {
+          DisableTelemetry = true;
+          ExtensionSettings = builtins.listToAttrs extensions;
 
-  home.file.".config/zen-browser/mozprefs.js".text = lib.concatStringsSep "\n" (lib.mapAttrsToList (
-      name: value: "lockPref(${lib.strings.toJSON name}, ${lib.strings.toJSON value});"
+          SearchEngines = {
+            Default = "ddg";
+            Add = [
+              {
+                Name = "nixpkgs packages";
+                URLTemplate = "https://search.nixos.org/packages?query={searchTerms}";
+                IconURL = "https://wiki.nixos.org/favicon.ico";
+                Alias = "@np";
+              }
+              {
+                Name = "NixOS options";
+                URLTemplate = "https://search.nixos.org/options?query={searchTerms}";
+                IconURL = "https://wiki.nixos.org/favicon.ico";
+                Alias = "@no";
+              }
+              {
+                Name = "NixOS Wiki";
+                URLTemplate = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
+                IconURL = "https://wiki.nixos.org/favicon.ico";
+                Alias = "@nw";
+              }
+              {
+                Name = "noogle";
+                URLTemplate = "https://noogle.dev/q?term={searchTerms}";
+                IconURL = "https://noogle.dev/favicon.ico";
+                Alias = "@ng";
+              }
+            ];
+          };
+        };
+      }
     )
-    prefs);
+  ];
 }
