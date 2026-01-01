5fd49c78f309cb7df71108e6f1d7c2fd8ba51b16
mugdad <mugdad02@tutamail.com>
2026-01-01 19:20:48 +0300

    d

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -123,7 +123,7 @@ in {
         layers_hog_keyboard_focus = true;
         initial_workspace_tracking = 0;
         mouse_move_enables_dpms = true;
-        key_press_enables_dpms = false;
+        key_press_enables_dpms = true;
         disable_hyprland_logo = true;
         disable_splash_rendering = true;
         enable_swallow = false;
